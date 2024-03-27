d53f34913ed79e47a6aaa9d2cc5a389e987ecc7c
quentinSoranzoKrebs <quentin.soranzokrebs@gmail.com>
2024-03-27 16:54:40 +0100

    update save 	modifié :         .flatpak-builder/checksums/x86_64-com.qsk.gconvert.json 	modifié :         com.qsk.gconvert.json 	modifié :         src/gconvert.gresource.xml 	modifié :         src/open.css 	modifié :         src/window.py 	modifié :         src/window.ui

--- src/window.py
+++ src/window.py
@@ -20,7 +20,23 @@
 from gi.repository import Adw
 from gi.repository import Gtk
 from PIL import Image
+from gconvert import main
 import os
+
+
+def afficher_contenu_repertoire(chemin):
+    # Vérifier si le chemin spécifié est un répertoire
+    if os.path.isdir(chemin):
+        # Liste tous les éléments dans le répertoire
+        contenu = os.listdir(chemin)
+
+        # Afficher chaque élément
+        for element in contenu:
+            print(element)
+    else:
+        print("Le chemin spécifié n'est pas un répertoire.")
+
+afficher_contenu_repertoire("/app/share/gconvert/gconvert")
 
 @Gtk.Template(resource_path='/com/qsk/gconvert/window.ui')
 class GconvertWindow(Adw.ApplicationWindow):
@@ -39,13 +55,23 @@ class GconvertWindow(Adw.ApplicationWindow):
         self.my_combobox.append_text("Option 1")
         self.my_combobox.connect("changed", self.on_my_combo_box_changed)
         self.button = Gtk.Button(label='Ouvrir un fichier')
-        self.button.connect('clicked', self.on_open_file_button_clicked)
+        self.button.connect('clicked', self.on_open_file_button_clicked1)
         self.Global_box.append(self.button)
 
-    def on_open_file_button_clicked(self, button):
-        dialog = Gtk.FileChooserDialog(title="Ouvrir un fichier", transient_for=self)
-        dialog.add_button("Annuler", Gtk.ResponseType.CANCEL)
-        dialog.add_button("Ouvrir", Gtk.ResponseType.ACCEPT)
+
+    def on_open_file_button_clicked1(self, button):
+        dialog = Gtk.FileChooserNative(title="Ouvrir un fichier", transient_for=self)
+        #dialog.add_button("Annuler", Gtk.ResponseType.CANCEL)
+        #dialog.add_button("Ouvrir", Gtk.ResponseType.ACCEPT)
+
+        # Filtre pour les images PNG, JPEG et vidéos MP4
+        filter_media = Gtk.FileFilter()
+        filter_media.set_name("Formats pris en charge")
+        filter_media.add_mime_type("image/png")
+        filter_media.add_mime_type("image/jpeg")
+        filter_media.add_mime_type("video/mp4")
+        dialog.add_filter(filter_media)
+
         dialog.connect("response", self.on_file_chooser_response)
         dialog.show()
 
@@ -59,19 +85,24 @@ class GconvertWindow(Adw.ApplicationWindow):
 
 
     def on_button_clicked1(self,widget):
+        # Ouvrir l'image à partir du chemin spécifié
         image = Image.open(self.file.get_path())
-        # Convertir l'image en noir et blanc
-        image_bw = image.convert('L')
+
+        # Convertir l'image en PNG
+        image_png = image.convert('RGB')
 
         # Définir le chemin de sauvegarde pour l'image convertie
         output_directory = "/home/quentin/Images"
-        output_filename = "image_convertie.jpg"
+        output_filename = "image_convet.png"
         output_path = os.path.join(output_directory, output_filename)
+
         # Enregistrer l'image convertie dans le répertoire spécifié
-        image_bw.save(output_path)
+        image_png.save(output_path, 'JPG')
 
     def on_button_clicked2(self,widget):
-        self.Global_box.set_orientation(Gtk.Orientation.VERTICAL)
+        dialog = Gtk.FileChooserNative(title="Ouvrir un fichier",action=Gtk.FileChooserAction.SAVE, transient_for=self)
+        dialog.connect("response", self.on_file_chooser_response)
+        dialog.show()
 
     def on_my_combo_box_changed(self,widget):
         selected_option = self.my_combobox.get_active_text()
